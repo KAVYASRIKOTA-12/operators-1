90ed690778cb5fbf3419c6dbf7086435cc74ba3e
KAVYASRIKOTA-12 <Kavyasrikota989@gmail.com>
2026-06-01 17:10:54 +0530

    Add files via upload

---
+++ Operators 1.py
@@ -0,0 +1,69 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#bitwise operators
+a=3
+b=4
+a&b
+0
+a=2
+b=5
+a&b
+0
+a=5
+b=7
+a&b
+5
+bin(3)
+'0b11'
+bin(4)
+'0b100'
+bin(5)
+'0b101'
+a=6
+b=8
+a&b
+0
+a=7
+b=9
+a|b
+15
+a=4
+>>> b=3
+>>> a|b
+7
+>>> a=9
+>>> a
+9
+`
+>>> a=9
+>>> ~a
+-10
+>>> a=6
+>>> ~a
+-7
+>>> a=6
+>>> b=3
+>>> a^b
+53
+>>> a=8
+>>> b=4
+>>> a^b
+12
+>>> a=4
+>>> a<<3
+32
+>>> a=5
+>>> a<<1
+10
+>>> a=4
+>>> a>>2
+1
+>>> a=5
+>>> a>>2
+1
+>>> a=8
+>>> a>>3
+1
+>>> a=5
+>>> a>>2
+1
